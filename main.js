0e590d7c3493f06cb245aceb687dbd3dc7ba07b8
Akash <technicalak282@gmail.com>
2024-01-13 22:03:54 +0530

    Design: Done with error handling and desktop design

---
+++ main.js
@@ -0,0 +1,30 @@
+function ValidateEmail(input) {
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (input.match(validRegex)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let email = document.querySelector("#email");
+let submitBtn = document.querySelector(".emailBtn");
+let signUpCard = document.querySelector(".signUpCard");
+let successCard = document.querySelector(".successCard");
+let error = document.querySelector(".error");
+let userEmail = document.querySelector("#userEmail");
+
+submitBtn.addEventListener("click", () => {
+  if (ValidateEmail(email.value)) {
+    signUpCard.style.display = "none";
+    successCard.style.display = "block";
+    userEmail.innerHTML = email.value;
+  } else {
+    error.style.display = "block";
+    email.style = ` background-color: rgba(255, 98, 87, 0.4);
+    border: 2px solid var(--Tomato);
+    outline: none;
+    border-radius: 5px;`;
+  }
+});
